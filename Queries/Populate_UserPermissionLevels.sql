d541e1dd0f9e0f6872b1f942767c2c0f84273fdc
Alex <alexpersiv@gmail.com>
2026-07-17 19:03:56 +0100

    populate user permission levels

---
+++ Queries/Populate_UserPermissionLevels.sql
@@ -0,0 +1,11 @@
+USE Library;
+
+INSERT INTO UserPermissionLevels (PermissionLevel)
+VALUES
+('None'),
+('Search only'),
+('Rent and return'),
+('Rent, Return, Register'),
+('Manage Library'),
+('Manage Library and Staff'),
+('Admin')
