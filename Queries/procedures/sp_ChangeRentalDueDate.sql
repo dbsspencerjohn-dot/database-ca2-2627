d3e5d69e65b257a63cabf1b4eeaf0e46fb598a31
Alex <alexpersiv@gmail.com>
2026-07-19 16:36:35 +0100

    create stored procedure change rental due date

---
+++ Queries/procedures/sp_ChangeRentalDueDate.sql
@@ -0,0 +1,34 @@
+-- adds more days on on a book's rental
+-- @RentalID as the ID of the rental record
+-- @RentalDays as a number of days to add to the rental record
+
+CREATE OR ALTER PROCEDURE sp_ChangeRentalDueDate
+    @RentalID INT
+    @RentalDays INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @CurRentStatus
+    DECLARE @CurDueDate
+    DECLARE @NewDueDate
+
+    SELECT @CurRentStatus = RentStatus, @CurDueDate = DueDate 
+    FROM RentRecords 
+    WHERE ID = @RentalID
+
+    SET @NewDueDate = DATEADD(day, @RentalDays, @CurDueDate)
+
+    UPDATE RentRecords 
+    SET DueDate = @NewDueDate
+    WHERE ID = @RentalID
+
+    -- Update from overdue to on time if date is extended
+    IF ( @CurRentStatus = (SELECT ID FROM RentalStatuses WHERE RentStatus = 'Overdue')
+        AND @NewDueDate > CONVERT(DATE, GETDATE()))
+        BEGIN
+            UPDATE RentRecords
+            SET RentStatus = (SELECT ID FROM RentalStatuses WHERE RentStatus = 'On Time')
+            WHERE ID = @RentalID
+        END;
+END;
--- Queries/procedures/sp_UpdateRentalStatus.sql
+++ Queries/procedures/sp_UpdateRentalStatus.sql
@@ -1,3 +1,8 @@
+-- update the status of a rental record
+-- @RentalID as the ID of the rental record
+-- @NewRentStatusText as the new status of the rental record
+-- as 'On Time', 'Overdue', 'Returned' or 'Book Lost'  
+
 CREATE OR ALTER PROCEDURE sp_UpdateRentalStatus
     @RentalID INT
     @NewRentStatusText VARCHAR(10)
